e05fef5134dace7a1ad7980a897ee8bca84b78c6
Jason Arritt <jsn.arritt@gmail.com>
2022-06-30 15:01:14 -0400

    Rough About component

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,17 +1,27 @@
 import React from "react";
-import Background from "../../assets/images/Background.jpg";
+// import Background from "../../assets/images/Background.jpg";
+import ProfilePic from "../../assets/images/Profile-Pic.jpg";
 
 function About() {
   return (
-    <section>
-      <h1 id="about">About Me</h1>
-      <img
-        src={Background}
-        className="my-2"
-        style={{ width: "100%" }}
-        alt="Background"
-      />
-    </section>
+    <div>
+      <section className="flex flex-row m-auto">
+        <h1 id="about">About Me</h1>
+        <img
+          src={ProfilePic}
+          className="my-1"
+          style={{ width: "10%" }}
+          alt="Profile Pic"
+        />
+      </section>
+      <section>
+        <p>
+          My name is Jason Arritt. I was previously a SD-WAN Technical
+          Consulting Engineer at Cisco. I am working on becoming a Full Stack
+          Developer.
+        </p>
+      </section>
+    </div>
   );
 }
 
