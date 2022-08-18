068d29dfe3b04ed9939083125755a2fce07dd892
Jason Arritt <jsn.arritt@gmail.com>
2022-08-18 13:16:07 -0400

    Updated BG and skills

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -14,24 +14,40 @@ function Resume() {
         <section>
           <h3>Front End Skills</h3>
           <ul>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>JavaScript</li>
-            <li>React</li>
+            <li>React.js</li>
+            <li>HTML5</li>
+            <li>CSS3</li>
+            <li>Bootstrap</li>
+            <li>Bulma</li>
+            <li>Handlebars.js</li>
           </ul>
         </section>
         <section>
           <h3>Back End Skills</h3>
           <ul>
             <li>Node.js</li>
-            <li>Express</li>
-            <li>SQL</li>
+            <li>Express.js</li>
             <li>MongoDB</li>
+            <li>MySQL</li>
+            <li>NoSQL</li>
+            <li>GraphQL</li>
+          </ul>
+        </section>
+        <section>
+          <h3>Languages / Other</h3>
+          <ul>
+            <li>Git / GitHub</li>
+            <li>JavaScript ES6+</li>
+            <li>JQuery</li>
+            <li>RESTful APIs</li>
+            <li>Jest</li>
+            <li>Heroku</li>
+            <li>Atlas</li>
           </ul>
         </section>
       </div>
       <section>
-        <p className="center">
+        <h4 className="center">
           <a
             href="https://docs.google.com/document/d/1sYYluQ0S-vzDAA_rU0BRdx4_DXUT4rWS/edit?usp=sharing&ouid=102039981224698088055&rtpof=true&sd=true"
             target="_blank"
@@ -39,7 +55,7 @@ function Resume() {
           >
             View Resume
           </a>
-        </p>
+        </h4>
       </section>
     </div>
   );
