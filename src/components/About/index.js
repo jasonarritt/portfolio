02d86d638a99fc2326953bc4e6d6cb8e44d96552
Jason Arritt <jsn.arritt@gmail.com>
2022-08-23 21:28:13 -0400

    About update

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -20,23 +20,32 @@ function About() {
       </section>
       <section>
         <p>
-          My name is Jason Arritt. I am originally from Southern California. In
-          my early twenties I joined the Army as an infantryman, which brought
-          me to North Carolina and I spent the following five years in the 82nd
-          Airborne Division out of Fort Bragg. After my time on active duty in
-          the Army, I was accepted into Cisco's Veteran Talent Incubation
-          Program, during which I earned my CCNA. Following the completion of
-          the six month VTIP course, in July of 2018 I began my employment at
-          Cisco as a Technical Consulting Engineer on the SD-WAN TAC team, and
-          also earned my CCNP Switch.
+          I currently live in Raleigh, North Carolina with my wife and our 3
+          dogs. I started my career by joining the Army as an infantryman, and
+          spent the following five years in the 82nd Airborne Division out of
+          Fort Bragg. I learned skills in leadership, responsibility, and team
+          building.
         </p>
         <p>
-          In July of 2022 I successfully completed the coding bootcamp at the
-          University of North Carolina at Chapel Hill. I have always had passion
-          for technology and learning as much as I can. I am currently seeking
-          employment as a full-stack web developer. I greatly enjoy seeking
-          solutions to problems and sharing the knowledge I have gained with
-          others.
+          After my time on active duty in the Army, I was accepted into Cisco's
+          Veteran Talent Incubation Program, during which I earned my CCNA.
+          Following the completion of the six month VTIP course, in July of
+          2018, I began my employment at Cisco as a Technical Consulting
+          Engineer on the SD-WAN TAC team. There I earned my CCNP Switch. I
+          gained skills in trouble shooting technological problems,
+          corresponding directly with customers, and working with a team.
+        </p>
+        <p>
+          I have always had a passion for technology and have a desire to learn
+          as much as I can. In July of 2022, I completed the coding bootcamp at
+          the University of North Carolina at Chapel Hill at the top of my
+          class.
+        </p>
+        <p>
+          I am currently seeking employment as a full-stack web developer. I
+          greatly enjoy seeking solutions to problems and sharing the knowledge
+          I have gained with others. I look forward to continuing to grow my
+          expertise in technology.
         </p>
       </section>
     </div>
